6e7516b79c2dd1eeffe929fe4b34947986e5156f
RogueProeliator <rp@rogueproeliator.com>
2024-06-14 22:06:29 -0500

    Converted classes to Python 3 compatibility

--- src/Sharp TV Network Remote.indigoPlugin/Contents/Server Plugin/plugin.py
+++ src/Sharp TV Network Remote.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1,111 +1,85 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
+#######################################################################################
 # Sharp TV Network Remote Control by RogueProeliator <rp@rogueproeliator.com>
-# 	Indigo plugin designed to allow full control of a Sharp AQUOS TV via the IP control
-#	protocol (wired or wireless network connection)
-#	
-#	Command structure based on Sharp's documentation, internet resources and discovered
-#	(unpublished) commands.
-#
-#	This plugin is released under an MIT license - this is a very simple and permissive
-#	license and may be found in the LICENSE.txt file found in the root of this plugin's
-#	GitHub repository:
-#		https://github.com/RogueProeliator/IndigoPlugins-Sharp-TV-Network-Remote
-#
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
+#######################################################################################
 
-
-#/////////////////////////////////////////////////////////////////////////////////////////
-# Python imports
-#/////////////////////////////////////////////////////////////////////////////////////////
+# region Python imports
 import indigo
 
-import RPFramework
+from RPFramework.RPFrameworkPlugin import RPFrameworkPlugin
 import sharpTvNetworkRemoteDevice
+#endregion
 
 
-#/////////////////////////////////////////////////////////////////////////////////////////
-# Constants and configuration variables
-#/////////////////////////////////////////////////////////////////////////////////////////
+class Plugin(RPFrameworkPlugin):
 
-
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
-# Plugin
-#	Primary Indigo plugin class that is universal for all devices (TV instances) to be
-#	controlled
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
-class Plugin(RPFramework.RPFrameworkPlugin.RPFrameworkPlugin):
-	
-	#/////////////////////////////////////////////////////////////////////////////////////
-	# Class construction and destruction methods
-	#/////////////////////////////////////////////////////////////////////////////////////
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	# Constructor called once upon plugin class creation; setup the device tracking
+	#######################################################################################
+	# region Class construction and destruction methods
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	# Constructor called once upon plugin class creation; set up the device tracking
 	# variables for later use
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	def __init__(self, plugin_id, plugin_display_name, plugin_version, plugin_prefs):
 		# RP framework base class's init method
-		super(Plugin, self).__init__(pluginId, pluginDisplayName, pluginVersion, pluginPrefs, managedDeviceClassModule=sharpTvNetworkRemoteDevice)
-	
-	
-	#/////////////////////////////////////////////////////////////////////////////////////
-	# MenuItem handlers
-	#/////////////////////////////////////////////////////////////////////////////////////
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+		super().__init__(plugin_id, plugin_display_name, plugin_version, plugin_prefs, managedDeviceClassModule=sharpTvNetworkRemoteDevice)
+
+	# endregion
+	#######################################################################################
+
+	#######################################################################################
+	# region MenuItem handlers
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This method will be called whenever the user has clicked the menus in order to
 	# enable the power-on setting for TV(s)
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def enablePowerOnCommands(self, valuesDict, typeId):
 		# this callback method must also do the validation... check to ensure that
 		# at least one TV has been selected from the list
-		errorsDict = indigo.Dict()
-		tvList = valuesDict.get(u'targetTVs')
+		errors_dict = indigo.Dict()
+		tv_list = valuesDict.get("targetTVs")
 		
-		if len(tvList) == 0:
-			errorsDict[u'targetTVs'] = u'Please select the TV(s) desired'
-			return (False, valuesDict, errorsDict)
+		if len(tv_list) == 0:
+			errors_dict["targetTVs"] = "Please select the TV(s) desired"
+			return False, valuesDict, errors_dict
 		else:
 			# log the fact that we are starting this command...
-			self.logger.info(u'Executing Enable Power On Commands...')
+			self.logger.info("Executing Enable Power On Commands...")
 			
 			# queue up the command for each selected device... this may be done via the
 			# execute action method
-			paramValues = indigo.Dict()
-			paramValues[u'powerOnMessageState'] = u'2'
-			for tvDevice in tvList:
-				self.executeAction(None, u'changeTVPowerOnMessages', int(tvDevice), paramValues)
-			return (True, valuesDict, indigo.Dict())
+			param_values = indigo.Dict()
+			param_values["powerOnMessageState"] = "2"
+			for tv_device in tv_list:
+				self.executeAction(None, "changeTVPowerOnMessages", int(tv_device), param_values)
+			return True, valuesDict, indigo.Dict()
 			
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine will be called from the user executing the menu item action to send
 	# an arbitrary command code to the Onkyo receiver
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-	
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def sendArbitraryCommand(self, valuesDict, typeId):
 		try:
-			deviceId = valuesDict.get(u'targetDevice', u'0')
-			commandCode = valuesDict.get(u'commandToSend', u'')
+			device_id = valuesDict.get("targetDevice", "0")
+			command_code = valuesDict.get("commandToSend", "")
 			
-			if deviceId == u'' or deviceId == u'0':
+			if device_id == "" or device_id == "0":
 				# no device was selected
-				errorDict = indigo.Dict()
-				errorDict["targetDevice"] = u'Please select a device'
-				return (False, valuesDict, errorDict)
-			elif commandCode == u'':
-				errorDict = indigo.Dict()
-				errorDict[u'commandToSend'] = u'Enter command to send'
-				return (False, valuesDict, errorDict)
+				error_dict = indigo.Dict()
+				error_dict["targetDevice"] = "Please select a device"
+				return False, valuesDict, error_dict
+			elif command_code == "":
+				error_dict = indigo.Dict()
+				error_dict["commandToSend"] = "Enter command to send"
+				return False, valuesDict, error_dict
 			else:
-				actionParams = indigo.Dict()
-				actionParams[u'commandCode'] = commandCode.ljust(8, ' ')
-				self.executeAction(pluginAction=None, indigoActionId=u'sendArbitraryCommand', indigoDeviceId=int(deviceId), paramValues=actionParams)
-				return (True, valuesDict)
+				action_params = indigo.Dict()
+				action_params["commandCode"] = command_code.ljust(8, ' ')
+				self.executeAction(pluginAction=None, indigoActionId="sendArbitraryCommand", indigoDeviceId=int(device_id), paramValues=action_params)
+				return True, valuesDict
 		except:
-			self.exceptionLog()
-			return (False, valuesDict)
-			
-	
+			self.logger.exception("Exception while executing send arbitrary command")
+			return False, valuesDict
+
+	# endregion
+	#######################################################################################
--- src/Sharp TV Network Remote.indigoPlugin/Contents/Server Plugin/sharpTvNetworkRemoteDevice.py
+++ src/Sharp TV Network Remote.indigoPlugin/Contents/Server Plugin/sharpTvNetworkRemoteDevice.py
@@ -1,94 +1,87 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
+#######################################################################################
 # Sharp TV Network Remote Control by RogueProeliator <rp@rogueproeliator.com>
-# 	See plugin.py for more plugin details and information
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
+#######################################################################################
 
-#/////////////////////////////////////////////////////////////////////////////////////////
-# Python imports
-#/////////////////////////////////////////////////////////////////////////////////////////
+# region  Python imports
 
 import indigo
-import RPFramework
+from RPFramework.RPFrameworkTelnetDevice import RPFrameworkTelnetDevice
+from RPFramework.RPFrameworkCommand import RPFrameworkCommand
 
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
-# SharpTvNetworkRemoteDevice
-#	Handles the configuration of a single Roku device that is connected to this plugin;
-#	this class does all the 'grunt work' of communications with the Roku
-#/////////////////////////////////////////////////////////////////////////////////////////
-#/////////////////////////////////////////////////////////////////////////////////////////
-class SharpTvNetworkRemoteDevice(RPFramework.RPFrameworkTelnetDevice.RPFrameworkTelnetDevice):
-	
-	#/////////////////////////////////////////////////////////////////////////////////////
-	# Class construction and destruction methods
-	#/////////////////////////////////////////////////////////////////////////////////////
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# endregion
+
+
+class SharpTvNetworkRemoteDevice(RPFrameworkTelnetDevice):
+
+	#######################################################################################
+	# region Class construction and destruction methods
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# Constructor called once upon plugin class receiving a command to start device
 	# communication. The plugin will call other commands when needed, simply zero out the
 	# member variables
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def __init__(self, plugin, device):
-		super(SharpTvNetworkRemoteDevice, self).__init__(plugin, device)
+		super().__init__(plugin, device)
+
+	# endregion
+	#######################################################################################
 		
-		
-	#/////////////////////////////////////////////////////////////////////////////////////
-	# Validation and GUI functions
-	#/////////////////////////////////////////////////////////////////////////////////////
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	#######################################################################################
+	# region Validation and GUI functions
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine is called to retrieve a dynamic list of elements for an action (or
 	# other ConfigUI based) routine
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	def getConfigDialogMenuItems(self, filter, valuesDict, typeId, targetId):
-		self.hostPlugin.logger.debug(u'SharpTV device received request for source list menu items')
-		availableInputs = []
-		availableInputs.append((u'RCKY36  ', u'Toggle Input'))
+		self.host_plugin.logger.debug("SharpTV device received request for source list menu items")
+		available_inputs = []
+		available_inputs.append(("RCKY36  ", "Toggle Input"))
 		
-		tvTunerLabel = self.indigoDevice.pluginProps.get(u'input0Label', u'')
-		if tvTunerLabel != u'':
-			availableInputs.append((u'ITVD0   ', tvTunerLabel))
+		tv_tuner_label = self.indigoDevice.pluginProps.get("input0Label", "")
+		if tv_tuner_label != "":
+			available_inputs.append(("ITVD0   ", tv_tuner_label))
 		
 		for i in range(1,9):
-			labelName = self.indigoDevice.pluginProps.get(u'input' + RPFramework.RPFrameworkUtils.to_unicode(i) + u'Label', u'')
-			if labelName != u'':
-				availableInputs.append((u'IAVD' + RPFramework.RPFrameworkUtils.to_unicode(i) + u'   ', labelName))
-		return availableInputs
-		
-		
-	#/////////////////////////////////////////////////////////////////////////////////////
-	# Processing and command functions
-	#/////////////////////////////////////////////////////////////////////////////////////
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+			label_name = self.indigoDevice.pluginProps.get(f"input{i}Label", "")
+			if label_name != "":
+				available_inputs.append((f"IAVD{i}   ", label_name))
+		return available_inputs
+
+	# endregion
+	#######################################################################################
+
+	#######################################################################################
+	# region Processing and command functions
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine will process the commands that are not processed automatically by the
 	# base class; it will be called on a concurrent thread
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def handleUnmanagedCommandInQueue(self, ipConnection, rpCommand):
-		if rpCommand.commandName == u'createTuneCommands':
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	def handle_unmanaged_command_in_queue(self, ip_connection, rp_command):
+		if rp_command.command_name == "createTuneCommands":
 			# this command payload will be the channel - either an analog of digital
 			# channel so we must parse that out
 			try:
-				fltChannel = float(rpCommand.commandPayload)
+				flt_channel = float(rp_command.command_payload)
 			except:
 				# this means an invalid tune command was received
-				self.hostPlugin.logger.warning(u'Ignored tune command to empty or invalid channel number')
+				self.host_plugin.logger.warning(u'Ignored tune command to empty or invalid channel number')
 				return
 				
-			if fltChannel <= 135:
+			if flt_channel <= 135:
 				# this is an analog tune command...
-				analogValuesDict = indigo.Dict()
-				analogValuesDict[u'channelNumber'] = rpCommand.commandPayload
-				self.hostPlugin.executeAction(None, u'tuneToAnalogChannel', self.indigoDevice.id, analogValuesDict)
+				analog_values_dict = indigo.Dict()
+				analog_values_dict["channelNumber"] = rp_command.command_payload
+				self.host_plugin.executeAction(None, "tuneToAnalogChannel", self.indigoDevice.id, analog_values_dict)
 			else:
-				self.hostPlugin.logger.warning(u'Digital channel tune not yet supported')
+				self.host_plugin.logger.warning("Digital channel tune not yet supported")
 		
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 	# This routine should return a touple of information about the connection - in the
 	# format of (ipAddress/HostName, portNumber)
-	#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-	def getDeviceAddressInfo(self):
-		return (self.indigoDevice.pluginProps.get(u'ipAddress', u''), int(self.indigoDevice.pluginProps.get(u'portNumber', u'10002')))
+	# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	def get_device_address_info(self):
+		return (self.indigoDevice.pluginProps.get("ipAddress", ""), int(self.indigoDevice.pluginProps.get("portNumber", "10002")))
 		
 		
